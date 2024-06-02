f0430fe1a448078af45736c305245995d40d2e13
abdrhmnsaeed <abdrhmnsaeed@gmail.com>
2024-06-02 22:54:42 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const increaseStep = () => setStep((step) => step + 1);
+  const decreaseStep = () => {
+    if (step > 1) setStep((step) => step - 1);
+  };
+
+  const increaseCount = () => setCount((count) => count + step);
+  const decreaseCount = () => setCount((count) => count - step);
+
+  const addDays = (date, days) => {
+    const newDate = new Date(date);
+    newDate.setDate(date.getDate() + days);
+    return newDate;
+  };
+  const todayDate = new Date();
+
+  const days = count;
+  const newDate = addDays(todayDate, days);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="count">
+        <button onClick={decreaseStep}>-</button>
+        <h3>Step: {step}</h3>
+        <button onClick={increaseStep}>+</button>
+      </div>
+
+      <div className="count">
+        <button onClick={decreaseCount}>-</button>
+        <h3>Count: {count}</h3>
+        <button onClick={increaseCount}>+</button>
+      </div>
+
+      <h2>
+        {count === 0
+          ? "Today"
+          : count === -1
+          ? `${count} days ago was`
+          : count > 0
+          ? `${count} days from today is`
+          : `${count} days ago was`}{" "}
+        {newDate.toDateString()}
+      </h2>
+    </>
   );
 }
-
-export default App;
